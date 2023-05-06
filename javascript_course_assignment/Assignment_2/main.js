7a49313e0adbd76135bdcb18cc40fe8101e13383
Moaz osama <moaz21-01130@student.eelu.edu.eg>
2023-05-06 12:22:05 +0300

    Assignment 2 Js

---
+++ javascript_course_assignment/Assignment_2/main.js
@@ -0,0 +1,45 @@
+//////////////////
+// Assignment 1 //
+//////////////////
+
+let numberOne = 10;
+let numberTwo = 20;
+
+console.log(numberOne+""+numberTwo);
+console.log(typeof (numberOne+""+numberTwo));
+// Another Way
+console.log(numberOne.toString() + numberTwo.toString());
+console.log(typeof (numberOne.toString() + numberTwo.toString()));
+
+console.log(`${numberOne}${numberTwo}`);
+console.log(typeof (`${numberOne}${numberTwo}`));
+console.log(numberTwo + 
+"\n" + numberOne);
+console.log(`${numberTwo}
+${numberOne}`);
+
+//////////////////
+// Assignment 2 //
+//////////////////
+
+console.log(elzero.innerHTML); // object
+console.log(typeof elzero); // object
+
+//////////////////
+// Assignment 3 //
+//////////////////
+
+console.log("`I'm In\n\\\\\nLove \\\\ \"\"\" '''\n++ With ++\n\\\"\"\"\\\"\"\"\n\"\"JavaScript\"\"``");
+
+//////////////////
+// Assignment 4 //
+//////////////////
+
+let a = 21;
+let b = 20;
+let c = b + "" + a
+
+console.log(`_${a}_${c}_${c}_${c}_${b}_`); // _21_2021_2021_2021_20_
+
+
+
