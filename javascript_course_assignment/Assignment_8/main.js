a3164e15880e62a1d3cd0e29527279605ca26cd3
Moaz osama <moaz21-01130@student.eelu.edu.eg>
2023-05-06 19:12:32 +0300

    Assignment 8 Js

---
+++ javascript_course_assignment/Assignment_8/main.js
@@ -0,0 +1,28 @@
+//////////////////
+// Assignment 1 //
+//////////////////
+
+
+let str = "   monday  ".trim();
+let day = str.charAt(0).toUpperCase() + str.slice(1);
+switch (day) {
+    case "Friday"  :
+    case "Saturday":
+    case"Sunday"   : 
+            console.log("No Appointments Available");
+            break;
+    case"Monday"  :
+    case"Thursday":
+            console.log("From 10:00 AM To 5:00 PM");
+            break;
+    case "Tuesday" : 
+            console.log("From 10:00 AM To 6:00 PM");
+            break;
+    case "Wednesday":
+            console.log("From 10:00 AM To 7:00 PM");
+            break;  
+    case "World" :
+            console.log("Its Not A Valid Day");
+            break;
+}
+
