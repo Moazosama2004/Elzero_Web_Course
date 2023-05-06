8ccac6112baa26157c195d83c9954f3df459fbd8
Moaz osama <moaz21-01130@student.eelu.edu.eg>
2023-05-06 13:48:46 +0300

    Assignment 3 Js

---
+++ javascript_course_assignment/Assignment_3/main.js
@@ -0,0 +1,63 @@
+//////////////////
+// Assignment 1 //
+//////////////////
+
+console.log((10 * 20) + 15 % 3 + (190 + 10) - 400); // 0
+
+
+//////////////////
+// Assignment 2 //
+//////////////////
+
+let num = 3;
+
+// Solution One
+console.log(num + num); // 6
+
+// Solution Two
+console.log(num * --num); // 6
+
+// Soultion Three
+console.log(++num + num ** true); // 6
+
+// Soultion Four
+console.log(num * (true + true)); // 6
+
+// Solution Five
+console.log(((num % --num) + true) * ++num); // 6
+
+// Solution Six
+console.log((--num) ** num + num); // 6
+
+
+//////////////////
+// Assignment 3 //
+//////////////////
+
+let number = "10";
+
+// Solution One
+console.log(+number + +number); // 20
+
+// Solution Two
+console.log(++number + (--number - true) ); // 20
+
+// Solution Three
+console.log(+number * (true * (true + true))); // 20
+
+// Solution Four
+console.log(+number * (true + true)); // 20
+
+//////////////////
+// Assignment 4 //
+//////////////////
+
+let points = 10;
+
+points += 3
+
+console.log(points); // 13
+
+points -= 5
+
+console.log(points); // 8;
